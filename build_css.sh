74e3922872ad0925549a73197926e99a55c1d136
Dominic Gisler <domgisler@gmail.com>
2019-08-11 14:16:14 +0200

    change css, added style to tinymce content styling

---
+++ build_css.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd public/css
+sass style.scss style.css
+yuicompressor style.css -o style.min.css
+sass content.scss content.css
+yuicompressor content.css -o content.min.css
+
+cd admin
+sass login.scss login.css
+yuicompressor login.css -o login.min.css
+sass style.scss style.css
+yuicompressor login.css -o login.min.css
