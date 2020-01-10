d07a36bd2db78192c009e01ea90caff7f35c6920
Dominic Gisler <domgisler@gmail.com>
2020-01-10 20:32:41 +0100

    added statistics graph to dashboard

--- src/Controller/Admin/IndexController.php
+++ src/Controller/Admin/IndexController.php
@@ -41,11 +41,17 @@ class IndexController extends AbstractController
             'platforms' => [],
             'browsers' => []
         ];
+        $graph = [
+            'sessions' => [],
+            'visits' => [],
+            'clients' => []
+        ];
 
         foreach ($clients as $client) {
             if ($client->getCreatedAt() != $client->getUpdatedAt()) {
                 $stats['counts']['real_clients']++;
             }
+            $graph['clients'] = $this->countDate($client->getCreatedAt(), $graph['clients']);
         }
 
         foreach ($visits as $visit) {
@@ -58,6 +64,8 @@ class IndexController extends AbstractController
                 ];
             }
             $stats['pages'][$pt->getPageTranslationId()]['visits']++;
+
+            $graph['visits'] = $this->countDate($visit->getCreatedAt(), $graph['visits']);
         }
 
         foreach ($sessions as $session) {
@@ -70,10 +78,97 @@ class IndexController extends AbstractController
                 $stats['browsers'][$session->getBrowser()] = 0;
             }
             $stats['browsers'][$session->getBrowser()]++;
+
+            $graph['sessions'] = $this->countDate($session->getCreatedAt(), $graph['sessions']);
         }
 
         return $this->render($request, $response, 'admin/index.twig', [
-            'stats' => $stats
+            'stats' => $stats,
+            'graph' => $this->mapGraphData($graph)
         ]);
+    }
+
+    /**
+     * @param string $date
+     * @param array $arr
+     * @return array
+     */
+    private function countDate(string $date, array $arr)
+    {
+        $timestamp = strtotime($date);
+        $times = [
+            'year' => date('Y', $timestamp),
+            'month' => date('Y-m', $timestamp),
+            'day' => date('Y-m-d', $timestamp),
+            'hour' => date('Y-m-d H:00', $timestamp),
+            'min' => date('Y-m-d H:i', $timestamp)
+        ];
+
+        foreach ($times as $key => $time) {
+            if (!isset($arr[$key][$time])) {
+                $arr[$key][$time] = 0;
+            }
+            $arr[$key][$time]++;
+        }
+
+        return $arr;
+    }
+
+    /**
+     * @param array $graph
+     * @return array
+     */
+    private function mapGraphData(array $graph)
+    {
+        $graphData = [];
+        foreach (['year', 'month', 'day', 'hour'] as $type) {
+            $graphData[$type] = [
+                'labels' => [],
+                'data' => [
+                    'clients' => [],
+                    'visits' => [],
+                    'sessions' => []
+                ]
+            ];
+        }
+
+        $types = [
+            'year' => [
+                'format' => 'Y',
+                'interval' => 10
+            ],
+            'month' => [
+                'format' => 'Y-m',
+                'interval' => 12
+            ],
+            'day' => [
+                'format' => 'Y-m-d',
+                'interval' => 30
+            ],
+            'hour' => [
+                'format' => 'Y-m-d H:00',
+                'interval' => 24
+            ]
+        ];
+        foreach ($types as $key => $cfg) {
+            for ($i = $cfg['interval']; $i >= 0; $i--) {
+                $graphData[$key]['labels'][] = date($cfg['format'], strtotime('-' . $i . $key . 's'));
+                $graphData[$key]['data']['clients'][] = 0;
+                $graphData[$key]['data']['sessions'][] = 0;
+                $graphData[$key]['data']['visits'][] = 0;
+            }
+        }
+
+        foreach ($graph as $type => $times) {
+            foreach ($times as $int => $counts) {
+                foreach ($counts as $key => $count) {
+                    $pos = array_search($key, $graphData[$int]['labels']);
+                    if ($pos) {
+                        $graphData[$int]['data'][$type][$pos] = $count;
+                    }
+                }
+            }
+        }
+        return $graphData;
     }
 }
