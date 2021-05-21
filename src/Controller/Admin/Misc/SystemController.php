f2a8bd34e9ac0b90d2efb73967b5a499ba1fce0f
Dominic Gisler <domgisler@gmail.com>
2021-05-21 16:55:19 +0200

    added some php ini configs to sysinfo

--- src/Controller/Admin/Misc/SystemController.php
+++ src/Controller/Admin/Misc/SystemController.php
@@ -189,6 +189,12 @@ class SystemController extends AbstractController
             ];
         }
 
+        $iniCheck = ['max_execution_time', 'max_input_time', 'memory_limit', 'post_max_size', 'upload_max_filesize'];
+        $phpConfigs = '';
+        foreach ($iniCheck as $key) {
+            $phpConfigs .= sprintf('%s: %s<br>', $key, ini_get($key));
+        }
+
         $data = [
             'CMS Version' => $cmsVersion,
             'PHP Version' => phpversion(),
@@ -197,6 +203,7 @@ class SystemController extends AbstractController
             'URL' => $this->get('base_url'),
             'Verwaltungs-URL' => $this->get('base_url') . $this->get('settings')['global']['admin_route'],
             'Verzeichnis' => $_SERVER['DOCUMENT_ROOT'],
+            'PHP Konfigurationen' => $phpConfigs,
             'PHP Erweiterungen' => '- ' . join('<br>- ', get_loaded_extensions()),
             'HTTP Protokoll' => $_SERVER['SERVER_PROTOCOL'],
             'Servername' => $_SERVER['SERVER_NAME'],
