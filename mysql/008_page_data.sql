8f0190e9d18b8a112fe821d23dd63cd37ca40832
Dominic Gisler <domgisler@gmail.com>
2021-06-04 17:35:38 +0200

    changed default data to include more examples

--- mysql/008_page_data.sql
+++ mysql/008_page_data.sql
@@ -1,10 +1,12 @@
 -- Einfügen der Standardseiten
 
 INSERT INTO `cms__page` (`page_id`, `name`, `enabled`, `fk_language_id`)
-VALUES (1, 'Startseite', 1, 1),
+VALUES (1, 'Startseite/Home', 1, 1),
        (2, 'Error 404', 0, 1),
-       (3, 'Impressum', 1, 1),
-       (4, 'Datenschutzerklärung', 1, 1);
+       (3, 'Impressum/Imprint', 1, 1),
+       (4, 'Datenschutzerklärung/Privacy policy', 1, 1),
+       (5, 'Galerie/Gallery', 1, 1),
+       (6, 'Gästebuch/Guestbook', 1, 1);
 
 INSERT INTO `cms__page_translation` (`page_translation_id`, `fk_page_id`, `fk_language_id`, `name`, `title`, `content`, `enabled`)
 VALUES (1, 1, 1, 'startseite', 'Startseite', '<h1>Willkommen auf deiner Webseite!</h1><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
@@ -12,7 +14,11 @@ VALUES (1, 1, 1, 'startseite', 'Startseite', '<h1>Willkommen auf deiner Webseite
 <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
 <p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
 <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.</p>', 1),
-       (2, 1, 2, 'home', 'Home', '<h1>Welcome to your Website!</h1>', 1),
+       (2, 1, 2, 'home', 'Home', '<h1>Welcome to your Website!</h1><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+<p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+<p>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.</p>', 1),
        (3, 2, 1, 'error-404', 'Error 404', '<p>Seite nicht gefunden!</p>', 1),
        (4, 2, 2, 'error-404', 'Error 404', '<p>Page not found!</p>', 1),
        (5, 3, 1, 'impressum', 'Impressum', '<h3>Kontakt-Adresse</h3>
@@ -27,8 +33,7 @@ VALUES (1, 1, 1, 'startseite', 'Startseite', '<h1>Willkommen auf deiner Webseite
 <h4>Urheberrechte</h4>
 <p>Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf dieser Website, geh&ouml;ren ausschliesslich <strong>Max Muster</strong> oder den speziell genannten Rechteinhabern. F&uuml;r die Reproduktion jeglicher Elemente ist die schriftliche Zustimmung des Urheberrechtstr&auml;gers im Voraus einzuholen.</p>
 <!--ACHTUNG: Wenn Sie die Quelle ohne Erlaubnis von SwissAnwalt entfernen, dann begehen Sie eine Urheberrechtsverletzung welche in jedem Fall geahndet wird.-->
-<p>Quelle: <a href="https://www.swissanwalt.ch" target="_blank" rel="noopener">SwissAnwalt</a></p>
-<!--Bitte beachten Sie die AGB von SwissAnwalt betreffend allf&auml;llig anfallenden Kosten bei Weglassen der Quelle!-->', 1),
+<p>Dieses Impressum basiert auf einer Vorlage von <a href="https://www.swissanwalt.ch" target="_blank" rel="noopener">SwissAnwalt</a>.</p>', 1),
        (6, 4, 1, 'datenschutz', 'Datenschutzerklärung', '<h2>Datenschutzerkl&auml;rung</h2>
 <p>Verantwortlich im Sinne des Datenschutzgesetzes:</p>
 <p>Max Muster<br />Musterweg 33<br />1234 Muster</p>
@@ -37,11 +42,11 @@ VALUES (1, 1, 1, 'startseite', 'Startseite', '<h1>Willkommen auf deiner Webseite
 <h4>Anmeldung auf unserer Webseite</h4>
 <p>Bei der Anmeldung f&uuml;r spezifische Angebote erfragen wir einige pers&ouml;nliche Daten wie Name, Anschrift, Kontakt, Telefonnummer oder E-Mail-Adresse. Angemeldete Nutzer k&ouml;nnen auf bestimmte Zusatzleistungen zugreifen. Angemeldete Nutzer haben die M&ouml;glichkeit, alle angegebenen pers&ouml;nlichen Daten zu jedem Zeitpunkt zu &auml;ndern oder l&ouml;schen zu lassen. Sie k&ouml;nnen auch jederzeit die von ihnen gespeicherten Daten bei uns erfragen. Soweit gesetzlich keine Frist f&uuml;r die Aufbewahrung der Daten besteht, k&ouml;nnen diese ge&auml;ndert oder gel&ouml;scht werden. Bitte kontaktieren Sie uns dazu &uuml;ber unsere Kontaktseite.</p>
 <h4>Kommentare</h4>
-<p>Wenn Sie auf unserer Webseite Kommentare abgeben werden neben dem reinen Text auch Angaben zum Zeitpunkt der Erstellung sowie ihr Nutzername gespeichert. Auf diese Art garantieren wir die Sicherheit unseres Blog, rechtswiedrige Beitr&auml;ge von Benutzern k&ouml;nnen nachverfolgt werden.</p>
+<p>Wenn Sie auf unserer Webseite Kommentare abgeben werden neben dem reinen Text auch Angaben zum Zeitpunkt der Erstellung sowie ihr Nutzername gespeichert. Auf diese Art garantieren wir die Sicherheit unseres Blog, rechtswidrige Beitr&auml;ge von Benutzern k&ouml;nnen nachverfolgt werden.</p>
 <h4>Kontaktformular</h4>
 <p>Wenn Sie uns &uuml;ber unsere Kontaktseite kontaktieren oder uns eine E-Mail senden werden die entsprechenden Daten zur Bearbeitung gespeichert.</p>
 <h4>L&ouml;schung von Daten</h4>
-<p>Ihre pers&ouml;nlichen Daten werden nur so lange gespeichert, wie dies absolut notwendig ist um die angegebenen Dienste zu leisten und es vom Gesetz vorgeschrieben ist. Nach Ablauf dieser Fristen werden die pers&ouml;nlichen Daten der Nutzer regelm&auml;&szlig;ig gel&ouml;scht.</p>
+<p>Ihre pers&ouml;nlichen Daten werden nur so lange gespeichert, wie dies absolut notwendig ist um die angegebenen Dienste zu leisten und es vom Gesetz vorgeschrieben ist. Nach Ablauf dieser Fristen werden die pers&ouml;nlichen Daten der Nutzer regelm&auml;ssig gel&ouml;scht.</p>
 <h4>Analyse der Aufrufe</h4>
 <p>Unsere Webseite erhebt Daten von Besuchern f&uuml;r statistische Zwecke, dies erfolgt &uuml;ber "Cookies".</p>
 <p>Die dadurch erhobenen Daten werden gespeichert, alle IP-Adressen direkt nach der Verarbeitung und vor der Speicherung anonymisiert. &Uuml;ber ihre Browser-Einstellungen k&ouml;nnen Sie die Verwendung von Cookies einstellen. Eventuell k&ouml;nnen diese Einstellungen Einfluss auf die korrekte Funktion unserer Website nehmen.</p>
@@ -54,7 +59,11 @@ VALUES (1, 1, 1, 'startseite', 'Startseite', '<h1>Willkommen auf deiner Webseite
 <p>Sie k&ouml;nnen zu jedem Zeitpunkt Informationen &uuml;ber Ihre bei uns gespeicherten Daten erbitten. Diese k&ouml;nnen auch berichtigt oder, sofern die vorgeschriebene Zeitr&auml;ume der Datenspeicherung zur Gesch&auml;ftsabwicklung abgelaufen sind, gel&ouml;scht werden. Gerne helfen wir Ihnen bei allen entsprechenden Fragen.</p>
 <p>&Auml;nderungen oder Widerruf von Einwilligungen k&ouml;nnen durch eine Mitteilung an uns vorgenommen werden. Dies ist auch f&uuml;r zuk&uuml;nftige Aktionen m&ouml;glich.</p>
 <h4>&Auml;nderung der Datenschutzbestimmungen</h4>
-<p>Unsere Datenschutzerkl&auml;rung kann in unregelm&auml;&szlig;igen Abst&auml;nden angepasst werden, damit sie den aktuellen rechtlichen Anforderungen entspricht oder um &Auml;nderungen unserer Dienstleistungen umzusetzen, z. B. bei der Einf&uuml;gung neuer Angebote. F&uuml;r Ihren n&auml;chsten Besuch gilt dann automatisch die neue Datenschutzerkl&auml;rung.</p>
+<p>Unsere Datenschutzerkl&auml;rung kann in unregelm&auml;ssigen Abst&auml;nden angepasst werden, damit sie den aktuellen rechtlichen Anforderungen entspricht oder um &Auml;nderungen unserer Dienstleistungen umzusetzen, z. B. bei der Einf&uuml;gung neuer Angebote. F&uuml;r Ihren n&auml;chsten Besuch gilt dann automatisch die neue Datenschutzerkl&auml;rung.</p>
 <h4>Kontakt</h4>
 <p>F&uuml;r Fragen zum Datenschutz senden Sie uns bitte eine Nachricht an info@example.com mit dem Betreff "Datenschutz".</p>
-<p>Diese Datenschutzerkl&auml;rung wurde bei <a href="https://xn--datenschutzerklrunggenerator-knc.de/" target="_blank" rel="noopener">datenschutzerkl&auml;runggenerator.de</a>&nbsp;erstellt.</p>', 1);
+<p>Diese Datenschutzerkl&auml;rung basiert auf einer Vorlage von <a href="https://xn--datenschutzerklrunggenerator-knc.de/" target="_blank" rel="noopener">datenschutzerkl&auml;runggenerator.de</a>.</p>', 1),
+       (7, 5, 1, 'galerie', 'Galerie', '<pre class="module">gallery-de</pre>', 1),
+       (8, 5, 2, 'gallery', 'Gallery', '<pre class="module">gallery-en</pre>', 1),
+       (9, 6, 1, 'gaestebuch', 'Gästebuch', '<pre class="module">guestbook-de</pre>', 1),
+       (10, 6, 2, 'guestbook', 'Guestbook', '<pre class="module">guestbook-en</pre>', 1);
--- mysql/019_add_navigation.sql
+++ mysql/019_add_navigation.sql
@@ -1,7 +1,8 @@
 -- Erstellt die standardmässige Navigation
 
 INSERT INTO `cms__module` (`name`, `enabled`, `trash`, `controller`, `config`)
-VALUES ('navigation', 1, 0, 'ConfigurationModuleController', '{
+VALUES
+('navigation-de', 1, 0, 'ConfigurationModuleController', '{
     "label": "Navigation",
     "items": [
         {
@@ -9,12 +10,197 @@ VALUES ('navigation', 1, 0, 'ConfigurationModuleController', '{
             "href": "/startseite"
         },
         {
-            "label": "Impressum",
-            "href": "/impressum"
+            "label": "Galerie",
+            "href": "/galerie"
         },
         {
-            "label": "Datenschutzerklärung",
-            "href": "/datenschutz"
+            "label": "Gästebuch",
+            "href": "/gaestebuch"
         }
     ]
+}'),
+('navigation-en', 1, 0, 'ConfigurationModuleController', '{
+    "label": "Navigation",
+    "items": [
+        {
+            "label": "Home",
+            "href": "/home"
+        },
+        {
+            "label": "Gallery",
+            "href": "/gallery"
+        },
+        {
+            "label": "Guestbook",
+            "href": "/guestbook"
+        }
+    ]
+}'),
+('gallery-de', 1, 0, 'GalleryModuleController', '{
+    "title": "Galerie",
+    "galleries": {
+        "demo1": {
+            "title": "Demo 1",
+            "description": "Demo Galerie 1",
+            "path": "gallery/demo1",
+            "cover": "gallery/demo1/pexels-holiho-1112186.jpg"
+        },
+        "demo2": {
+            "title": "Demo 2",
+            "description": "Demo Galerie 2",
+            "path": "gallery/demo2",
+            "cover": "gallery/demo2/pexels-pixabay-235621.jpg"
+        }
+    }
+}'),
+('gallery-en', 1, 0, 'GalleryModuleController', '{
+    "title": "Gallery",
+    "galleries": {
+        "demo1": {
+            "title": "Demo 1",
+            "description": "Demo Gallery 1",
+            "path": "gallery/demo1",
+            "cover": "gallery/demo1/pexels-holiho-1112186.jpg"
+        },
+        "demo2": {
+            "title": "Demo 2",
+            "description": "Demo Gallery 2",
+            "path": "gallery/demo2",
+            "cover": "gallery/demo2/pexels-pixabay-235621.jpg"
+        }
+    }
+}'),
+('guestbook-de', 1, 0, 'GuestbookModuleController', '{
+    "identifier": "guestbook",
+    "notification": {
+        "enable": false,
+        "mailer": {
+            "smtp": false,
+            "host": "mail.example.com",
+            "smtpauth": true,
+            "username": "max.muster@example.com",
+            "password": "mypass",
+            "smtpsecure": "ssl",
+            "port": 465
+        },
+        "from": {
+            "email": "max.muster@example.com",
+            "name": "Max Muster"
+        },
+        "to": {
+            "email": "max.muster@example.com",
+            "name": "Max Muster"
+        }
+    },
+    "elements": {
+        "name": {
+            "label": "Name",
+            "placeholder": "Name",
+            "type": "text",
+            "required": true,
+            "validators": {
+                "string_length": {
+                    "min": 3,
+                    "max": 20
+                }
+            }
+        },
+        "message": {
+            "type": "textarea",
+            "label": "Nachricht",
+            "placeholder": "Nachricht",
+            "hint": "Ihre Nachricht",
+            "required": true,
+            "validators": {
+                "string_length": {
+                    "min": 10,
+                    "max": 500
+                }
+            }
+        },
+        "send": {
+            "type": "submit",
+            "label": "Senden",
+            "class": "btn-primary"
+        },
+        "reset": {
+            "type": "reset",
+            "label": "Abbrechen",
+            "class": "btn-secondary"
+        }
+    },
+    "messages": {
+        "error": "Bitte überprüfe deine Eingaben",
+        "success": "Eintrag wurde gespeichert",
+        "failed": "Es ist ein Fehler aufgetreten, bitte versuche es später erneut",
+        "empty": "Bisher keine Einträge",
+        "timestamp": "am %s um %s"
+    }
+}'),
+('guestbook-en', 1, 0, 'GuestbookModuleController', '{
+    "identifier": "guestbook",
+    "notification": {
+        "enable": false,
+        "mailer": {
+            "smtp": false,
+            "host": "mail.example.com",
+            "smtpauth": true,
+            "username": "max.muster@example.com",
+            "password": "mypass",
+            "smtpsecure": "ssl",
+            "port": 465
+        },
+        "from": {
+            "email": "max.muster@example.com",
+            "name": "Max Muster"
+        },
+        "to": {
+            "email": "max.muster@example.com",
+            "name": "Max Muster"
+        }
+    },
+    "elements": {
+        "name": {
+            "label": "Name",
+            "placeholder": "Name",
+            "type": "text",
+            "required": true,
+            "validators": {
+                "string_length": {
+                    "min": 3,
+                    "max": 20
+                }
+            }
+        },
+        "message": {
+            "type": "textarea",
+            "label": "Message",
+            "placeholder": "Message",
+            "hint": "Your message",
+            "required": true,
+            "validators": {
+                "string_length": {
+                    "min": 10,
+                    "max": 500
+                }
+            }
+        },
+        "send": {
+            "type": "submit",
+            "label": "Send",
+            "class": "btn-primary"
+        },
+        "reset": {
+            "type": "reset",
+            "label": "Cancel",
+            "class": "btn-secondary"
+        }
+    },
+    "messages": {
+        "error": "Please check your entries",
+        "success": "Your entry has been saved",
+        "failed": "An error occurred, please try again later",
+        "empty": "No entries yet",
+        "timestamp": "at %s on %s"
+    }
 }');
