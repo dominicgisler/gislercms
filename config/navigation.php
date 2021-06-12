7624e5371c7ee84cab455c1baa8896f2c977acbd
Dominic Gisler <domgisler@gmail.com>
2021-06-12 12:56:05 +0200

    changed navigation

--- config/navigation.php
+++ config/navigation.php
@@ -48,72 +48,57 @@ return [
                 ]
             ],
             [
-                'title' => 'Seiten',
+                'title' => 'Inhalte',
                 'link' => [
                     'icon' => 'fa fa-plus',
-                    'href' => '{admin_url}/page/add'
+                    'href' => [
+                        'Seite' => '{admin_url}/page/add',
+                        'Beitrag' => '{admin_url}/post/add',
+                        'Widget' => '{admin_url}/widget/add',
+                        'Modul' => '{admin_url}/module/add'
+                    ]
                 ],
                 'items' => [
                     [
                         'route' => ['admin-page-list', 'admin-page-edit'],
-                        'label' => 'Alle Seiten',
-                        'icon' => 'fa-copy',
+                        'label' => 'Seiten',
+                        'icon' => 'fa-file-alt',
                         'href' => '{admin_url}/page/list',
                         'count' => '{pages_count}'
+                    ],
+                    [
+                        'route' => ['admin-post-list', 'admin-post-edit'],
+                        'label' => 'Beiträge',
+                        'icon' => 'fa-rss',
+                        'href' => '{admin_url}/post/list',
+                        'count' => '{posts_count}'
+                    ],
+                    [
+                        'route' => ['admin-widget-list', 'admin-widget-edit'],
+                        'label' => 'Widgets',
+                        'icon' => 'fa-sticky-note',
+                        'href' => '{admin_url}/widget/list',
+                        'count' => '{widgets_count}'
+                    ],
+                    [
+                        'route' => ['admin-module-list', 'admin-module-add', 'admin-module-manage'],
+                        'label' => 'Module',
+                        'icon' => 'fa-code',
+                        'href' => '{admin_url}/module/list',
+                        'count' => '{modules_count}'
                     ],
                     [
                         'route' => 'admin-page-defaults',
                         'label' => 'Standardwerte',
                         'icon' => 'fa-cog',
                         'href' => '{admin_url}/page/defaults'
-                    ]
-                ]
-            ],
-            [
-                'title' => 'Beiträge',
-                'link' => [
-                    'icon' => 'fa fa-plus',
-                    'href' => '{admin_url}/post/add'
-                ],
-                'items' => [
+                    ],
                     [
-                        'route' => ['admin-post-list', 'admin-post-edit'],
-                        'label' => 'Alle Beiträge',
-                        'icon' => 'fa-copy',
-                        'href' => '{admin_url}/post/list',
-                        'count' => '{posts_count}'
-                    ]
-                ]
-            ],
-            [
-                'title' => 'Widgets',
-                'link' => [
-                    'icon' => 'fa fa-plus',
-                    'href' => '{admin_url}/widget/add'
-                ],
-                'items' => [
-                    [
-                        'route' => ['admin-widget-list', 'admin-widget-edit'],
-                        'label' => 'Alle Widgets',
-                        'icon' => 'fa-copy',
-                        'href' => '{admin_url}/widget/list',
-                        'count' => '{widgets_count}'
-                    ]
-                ]
-            ],
-            [
-                'title' => 'Module',
-                'link' => [
-                    'icon' => 'fa fa-plus',
-                    'href' => '{admin_url}/module/add'
-                ],
-                'items' => [
-                    [
-                        'route' => ['admin-module-list', 'admin-module-manage'],
-                        'label' => 'Alle Module',
-                        'icon' => 'fa-copy',
-                        'href' => '{admin_url}/module/list',
-                        'count' => '{modules_count}'
+                        'route' => 'admin-misc-trash',
+                        'label' => 'Papierkorb',
+                        'icon' => 'fa-trash-alt',
+                        'href' => '{admin_url}/misc/trash',
+                        'count' => '{trash_count}'
                     ]
                 ]
             ],
@@ -151,13 +136,6 @@ return [
                         'label' => 'Benutzer',
                         'icon' => 'fa-users',
                         'href' => '{admin_url}/misc/user/list'
-                    ],
-                    [
-                        'route' => 'admin-misc-trash',
-                        'label' => 'Papierkorb',
-                        'icon' => 'fa-trash-alt',
-                        'href' => '{admin_url}/misc/trash',
-                        'count' => '{trash_count}'
                     ],
                     [
                         'route' => 'admin-misc-license',
