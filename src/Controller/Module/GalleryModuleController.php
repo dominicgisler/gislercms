1895c16912cb19041c90993acce2431d1ecfb5f9
Dominic Gisler <domgisler@gmail.com>
2024-10-08 17:45:49 +0200

    added option to download gallery

--- src/Controller/Module/GalleryModuleController.php
+++ src/Controller/Module/GalleryModuleController.php
@@ -6,6 +6,7 @@ use GislerCMS\Controller\Admin\Module\Manage\GalleryController;
 use GislerCMS\Model\PageTranslation;
 use Slim\Http\Request;
 use Twig\Error\LoaderError;
+use ZipArchive;
 
 /**
  * Class GalleryModuleController
@@ -23,13 +24,15 @@ class GalleryModuleController extends AbstractModuleController
                 'title' => 'Test 1',
                 'description' => '',
                 'path' => 'gallery/test01',
-                'cover' => 'gallery/test01/cover.jpg'
+                'cover' => 'gallery/test01/cover.jpg',
+                'download' => true
             ],
             'test02' => [
                 'title' => 'Test 2',
                 'description' => '',
                 'path' => 'gallery/test02',
-                'cover' => 'gallery/test02/cover.jpg'
+                'cover' => 'gallery/test02/cover.jpg',
+                'download' => false
             ]
         ]
     ];
@@ -81,5 +84,39 @@ class GalleryModuleController extends AbstractModuleController
         }
 
         return $html;
+    }
+
+    /**
+     * @param Request $request
+     * @return string
+     * @throws LoaderError
+     */
+    public function onPost(Request $request): string
+    {
+        $gallery = $request->getAttribute('arguments');
+
+        if (empty($gallery) || !isset($this->config['galleries'][$gallery]) || !$this->config['galleries'][$gallery]['download']) {
+            return $this->onGet($request);
+        }
+
+        $filename = tempnam(sys_get_temp_dir(), 'gallery-export');
+
+        $zip = new ZipArchive();
+        $zip->open($filename, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+        $galPath = realpath(__DIR__ . '/../../../public/uploads/' . $this->config['galleries'][$gallery]['path']);
+        foreach (glob($galPath . '/*.*') as $file) {
+            $relPath = substr($file, strlen($galPath) + 1);
+            $zip->addFile($file, $relPath);
+        }
+
+        $zip->close();
+
+        header('Content-Type: application/zip');
+        header('Content-Disposition: attachment; filename=' . $gallery . '.zip');
+        readfile($filename);
+        unlink($filename);
+
+        return '';
     }
 }
